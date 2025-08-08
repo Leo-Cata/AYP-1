c6f01c0bc1133329c14242f72287fb80bb787ed6
leo <leonel.m.cata03@gmail.com>
2025-08-08 21:41:49 +0100

    ej2 tp1

---
+++ TP1/ej2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+    char nombre[]= "";
+    printf("¿Cual es tu nombre?\n");
+    scanf("%s", &nombre);
+
+    printf("Hola %s \n", nombre);
+
+}
---
+++ hola_mundo.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    char name[]="";
+    printf("ingresar tu nombre  ");
+    scanf("%s", &name );
+    printf("hola %s",name);
+}
