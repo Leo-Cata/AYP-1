e5dfb7d91686c57a7e2cfae510b9ab7abf91efe4
leo <leonel.m.cata03@gmail.com>
2025-08-20 22:42:36 +0100

    ej12

---
+++ TP1/ej12.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int numeroUno, numeroDos, resultado;
+
+    printf("ingrese dos numeros enteros para multiplicar\n");
+    scanf("%i %i", &numeroUno, &numeroDos);
+
+    for (int i = 0; i < numeroDos; i++){
+        resultado = resultado + numeroUno;
+    }
+
+    printf("El resultado de la multiplicacion mediante suma es: %i\n", resultado);
+}
