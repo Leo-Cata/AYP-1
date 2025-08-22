049c47a8475073ab17c4c826143e049dfa631035
leo <leonel.m.cata03@gmail.com>
2025-08-22 23:16:08 +0100

    ej18 wip

--- TP1/ej18.c
+++ TP1/ej18.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 
 int main(){
-    
+    int numberToCheck, reversedNumberToCheck;
+
+    printf("Ingrese un numero para ver si es capicua\n");
+    scanf("%i", &numberToCheck);
+
+    int temp_number= numberToCheck;
+    while(temp_number > 0){
+        int digit = temp_number % 10;
+
+        reversedNumberToCheck= reversedNumberToCheck * 10 + digit;
+
+        temp_number= temp_number/10;
+    }
+
+    if(numberToCheck == reversedNumberToCheck){
+        printf("es capicua");
+    }else{
+        printf("No es capicua");
+    }
 }
