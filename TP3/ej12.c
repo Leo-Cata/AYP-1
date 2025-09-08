6468eaba36e4bfec411374f888baf9cbfdb16f50
leo <leonel.m.cata03@gmail.com>
2025-09-08 22:04:37 +0100

    ej12 terminado

--- TP3/ej12.c
+++ TP3/ej12.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -6,11 +7,18 @@
 #define nameSize 50
 #define arraySize 100
 
+// union para guardar un tipo u otro
+union Stock {
+    int unidades;
+    float peso;
+};
+
 // catalogo de productos
 struct Producto {
     char codigo[codeSize];
     char nombre[nameSize];
-    int stock;
+    int stockType; // 0 para unidades, 1 para kilos
+    union Stock stock;
     float precio;
 };
 
@@ -24,17 +32,11 @@ void replaceWithArrayEnd(char* array)
     }
 }
 
-void cleanBuffer()
-{
-    while (getchar() != '\n')
-        ;
-}
-
 // funcion para agregar un producto
 void addProduct(int* counter, struct Producto* catalogo)
 {
     // limpia el buffer
-    cleanBuffer();
+    getchar();
 
     // logica para ingresar el codigo de un nuevo producto, asignarlo y reemplazar el salto por finalizacion de la cadena
     char newProductCode[codeSize];
@@ -49,9 +51,29 @@ void addProduct(int* counter, struct Producto* catalogo)
     replaceWithArrayEnd(newProductName);
 
     // logica para ingresar el stock de un nuevo producto, asignarlo
-    int newProductStock = 0;
-    printf("ingrese el stock registrar\n");
-    scanf("%i", &newProductStock);
+    int newProductStockType;
+    printf("para ingresar el stock, ingrese 1 para unidades, y 2 para el peso\n");
+    scanf("%d", &newProductStockType);
+
+    // inicializacion de las variables para los 2 tipos de stock
+    // luego dependiendo si anteriormente se ingreso 0 para unidados
+    // o 1 para peso (kilo/litros), se vuelva a preguntar para
+    // ingresar un y guardar el respectivo stock
+    int newProductStockUnits = 0;
+    float newProductStockWeight = 0.0;
+
+    // dependiendo del tipo de stock ingresado, pide un valor
+    // si el tipo de stock ingresado es invalido, termina la funcion tirando error
+    if (newProductStockType == 1) {
+        printf("ingrese la cantidad de unidades enteras\n");
+        scanf("%d", &newProductStockUnits);
+    } else if (newProductStockType == 2) {
+        printf("ingrese el peso como KG.GR O L.ML\n");
+        scanf("%f", &newProductStockWeight);
+    } else {
+        printf("tipo de stock invalido\nla operacion se cancelara\n");
+        return;
+    }
 
     // logica para ingresar el precio de un nuevo producto, asignarlo
     float newProductPrice = 0.0;
@@ -64,8 +86,18 @@ void addProduct(int* counter, struct Producto* catalogo)
     if (*counter < arraySize) {
         strcpy(catalogo[*counter].codigo, newProductCode);
         strcpy(catalogo[*counter].nombre, newProductName);
-        catalogo[*counter].stock = newProductStock;
         catalogo[*counter].precio = newProductPrice;
+        catalogo[*counter].stockType = newProductStockType;
+
+        // si el tipo de producto es 0/1, guarda el valor es su respectivo parametro
+        if (newProductStockType == 1) {
+            catalogo[*counter].stock.unidades = newProductStockUnits;
+        } else if (newProductStockType == 2) {
+            catalogo[*counter].stock.peso = newProductStockWeight;
+        }
+        printf("El producto se ha guardado\n");
+
+        // aumenta para registrar cauntos productos hay en total
         (*counter)++;
     } else {
         printf("Se ha superado el numero maximo de productos\n");
@@ -76,7 +108,7 @@ void addProduct(int* counter, struct Producto* catalogo)
 void searchProduct(int* counter, struct Producto* catalogo)
 {
     // limpia el buffer
-    cleanBuffer();
+    getchar();
 
     // logica para ingresar el nombre de un producto
     // asigarlo a una variable y agregar \0 para denotar que se termino la cadena
@@ -87,24 +119,27 @@ void searchProduct(int* counter, struct Producto* catalogo)
 
     // for loop el cual busca desde el comienso hasta el ultimo valor del contador
     // un producto con el mismo nombre e imprime sus datos
-    int foundProduct = 0;
-    for (int i = 0; i < *counter; i++) {
+    bool foundProduct = false;
+    for (int i = 0; i < *counter && !foundProduct; i++) {
         // compara si el nombre del producto en posicion [i], es igual al producto ingresado
         // y luego lo imprime y setea la variable de encontrado
         if (strcmp(catalogo[i].nombre, productNameToSearch) == 0) {
-            printf("Producto encontrado: %s %s %d %.3f\n",
+            printf("Producto encontrado\ncodigo: %s, nombre: %s, precio: %.3f,  ",
                 catalogo[i].codigo,
                 catalogo[i].nombre,
-                catalogo[i].stock,
                 catalogo[i].precio);
+            if (catalogo[i].stockType == 1) {
+                printf("unidades: %d\n", catalogo[i].stock.unidades);
+            } else {
+                printf("peso: %.2fK/L", catalogo[i].stock.peso);
+            }
             printf("\n");
-            foundProduct = 1;
-            break;
+            foundProduct = true;
         }
     }
 
     // si no hay resultados, imprime el error
-    if (foundProduct == 0) {
+    if (!foundProduct) {
         printf("el producto no fue encontrado\n");
     }
 };
@@ -112,14 +147,20 @@ void searchProduct(int* counter, struct Producto* catalogo)
 // funcion para imprimir todo el catalogo
 void printCatalog(int* counter, struct Producto* catalogo)
 {
+    printf("productos:\n");
     // loopea desde 0 hasta el ultimo numero de productos registrado por el programa
     for (int i = 0; i < *counter; i++) {
 
-        printf("Productos: %s %s %d %.3f\n",
+        printf("codigo: %s, nombre: %s, precio: %.3f, ",
             catalogo[i].codigo,
             catalogo[i].nombre,
-            catalogo[i].stock,
             catalogo[i].precio);
+        // si el tipo de stock son unidades (1), o peso (2), imprime
+        if (catalogo[i].stockType == 1) {
+            printf("unidades: %d\n", catalogo[i].stock.unidades);
+        } else {
+            printf("peso: %.2f KG.GR/L.ML\n", catalogo[i].stock.peso);
+        }
     }
 }
 
@@ -132,7 +173,7 @@ int main()
     int counter = 0;
 
     // while loop para ejecutar todos los comandos o finalizar
-    while (1) {
+    while (true) {
 
         // inicializacion de variables para saber que desea el usuario hacer
         // y contador para el array de productos
