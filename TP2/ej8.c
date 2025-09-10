cce03fda2c0c3642b7ac49e7b2a5ce2ae68e31c0
Leo <leonel.m.cata03@gmail.com>
2025-09-10 23:31:51 +0100

    ej8 tp2

--- TP2/ej8.c
+++ TP2/ej8.c
@@ -1,11 +1,56 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /*
 Escriba un programa que a partir de un arreglo de 10 fechas (representadas como cadenas de
 caracteres) hardcoded indique la mayor y la menor.
 */
 
+// funcion la cual toma una fecha y convierte sus partes a int, devolviendo la fecha en formato yyyymmdd para luego comparar
+int stringDatesToInt(char* date)
+{
+    // variable para guardar temporalmente la fecha  y luego la pocia a esa variable
+    char temporalString[11];
+    strcpy(temporalString, date);
+
+    // variable para almacenar la dir en memoria del resultado de strtok
+    char* dividedStringToken = strtok(temporalString, "/");
+
+    // variables para guarda las fechas
+    //  atoi= ascii to int
+    int day, month, year;
+    day = atoi(dividedStringToken);
+
+    dividedStringToken = strtok(NULL, "/");
+    month = atoi(dividedStringToken);
+
+    dividedStringToken = strtok(NULL, "/");
+    year = atoi(dividedStringToken);
+
+    return year * 10000 + month * 100 + day;
+};
+
 int main()
 {
-    char datesArray[10][10] = { "01 " };
-}
+    char datesArray[10][11] = { "15/03/2020", "20/12/199", "01/01/2023", "10/06/2018", "25/12/2022", "03/07/2021", "14/02/2017", "08/09/2024", "30/11/2015", "12/04/2016" };
+
+    // biggerDate tiene que ser 0, la cual seria la fecha mas chica
+    //  smallerdate tiene que ser 9999 99 99 para que sea la fecha mas grande posible
+    int biggerDate = 0;
+    int smallerDate = 99999999;
+
+    // loopea por todas las partes del array
+    //  si biggerdate mas grande es menor a lo que devuelve la guarda
+    //  si smallerdate es mayor a lo que devuelva la funcion, la guarda
+    for (int i = 0; i < 10; i++) {
+        if (biggerDate < stringDatesToInt(datesArray[i])) {
+            biggerDate = stringDatesToInt(datesArray[i]);
+        }
+        if (smallerDate > stringDatesToInt(datesArray[i])) {
+            smallerDate = stringDatesToInt(datesArray[i]);
+        }
+    }
+
+    printf("la fecha mas grande es: %d, y la fecha chica es: %d", biggerDate, smallerDate);
+};
--- TP2/entregable11.c
+++ TP2/entregable11.c
@@ -38,7 +38,7 @@ int main()
     char userSentence[sentenceLength], dictionary[arrayLength][sentenceLengthArray] = { "la", "de", "que", "el", "en", "y", "a", "los", "se", "del", "las", "por", "un", "con", "no", "una", "su", "para", "es", "al", "lo", "como", "más", "o", "pero", "sus", "le", "han", "sin", "sobre", "este", "ya", "entre", "cuando", "todo", "esta", "ser", "son", "dos", "también", "fue", "había", "era", "muy", "años", "hasta", "desde", "está", "mi", "porque", "él", "cuando", "tú", "sí", "nos", "ni", "contra", "tú", "él", "ella", "ellos", "nosotros", "vosotros", "ellos", "sí", "no", "cada", "otro", "sus", "mi", "nuestro", "tu", "su", "sus", "nuestro", "nuestra", "nuestros", "nuestras", "vuestro", "vuestra", "vuestros", "vuestras", "este", "esta", "estos", "estas", "aquel", "aquella", "aquellos", "aquellas", "alguno", "alguna", "algunos", "algunas", "ninguno", "ninguna", "ningunos", "ningunas", "otro", "otra" };
 
     // inicializacion de variable que contiene el numero de la cantidad de palabras
-    //  harcodeades en el diccionario
+    // harcodeades en el diccionario
     int dictionaryCount = 87;
 
     // while loop que corre durante toda la ejecucion hasta que se escriba la palabra fin
@@ -51,28 +51,34 @@ int main()
         // funcion que reemplaza el salto de linea con \0 para denotar el termino del array
         replaceWithArrayEnd(userSentence);
 
-        // si ACA ME QUEDE
+        // compara si la palabra ingresada fue fin, y finaliza el while loop
         if (strcmp(userSentence, "fin") == 0) {
             break;
         }
 
-        // loopea por el array y chequea si la palabra se encuentra
-        char* words[sentenceLengthArray * 3];
+        // variable puntero en el cual se separa la oracion ingresada por el usuario en array de palabras singulares
+        char* words[sentenceLength];
+
+        // inicializacion de la cantidad de palabras, 0 por defecto
         int wordCount = 0;
-        // toma la oracion, busca uno de los limitadores que estan entre " "
-        // devuelve el punto a la primera palabra valida
+
+        // toma la oracion ingresada por le usuario y busca los primeros caracteres validos hasta que encuentra " ,.;:!?"
+        // y la asigna como ultima palabra validad
         char* lastValidWord = strtok(userSentence, " ,.;:!?");
 
         // mientras que los la ultima palabra valida no sean null
-        // y la cantidad de palabras sea menor al largo maximo de la oracion
-        while (lastValidWord != NULL && wordCount < sentenceLengthArray * 3) {
-            // para la palabra en words, con posicion wordcount
+        // y la cantidad de palabras ingresada sea menor al largo maximo de la oracion (100 caracteres)
+        while (lastValidWord != NULL && wordCount < sentenceLength) {
+
+            // para la palabra en words, con posicion wordcount (de 0 hasta 100)
             // le asigna bytes en memoria igual al largo de la ultima palabra mas uno
             // para el caracter nulo
             words[wordCount] = malloc(strlen(lastValidWord) + 1);
 
             // copia la ultima palabra valida en la ultima posicion
             strcpy(words[wordCount], lastValidWord);
+
+            // suma al contador de palabras
             wordCount++;
 
             // le asigna la ultima palabra valida a la variable
@@ -82,52 +88,68 @@ int main()
 
         // si la palabra ingresada por el usario no esta vacia
         if (userSentence != NULL) {
+
+            // inicializacion de variable para contar cuando se encuentra una palabra repetida
             int wordFoundInDictionary = 0;
-            // muestra las palabras separadas
-            printf("\npalabras separadas\n");
+
+            // doble for loop, para tomar la primera palabra ingresada por el usuario [i], y chequear por todo el diccionario [j]
+            // antes de pasar a la siguiente palabra
             for (int i = 0; i < wordCount; i++) {
+
+                // setea la palabra encontrada en 0 para cada ciclo de i
+                // osea para cada nueva palabra en el array de palabras ingresadas por el usuario
                 wordFoundInDictionary = 0;
-                printf("\npalabra %d: %s\n", i + 1, words[i]);
 
                 // loopea chequeando si la palabra en posicion i se encuentra en
                 // alguno de los lugares en j, devuelvo 0 si se encontro
                 for (int j = 0; j < arrayLength; j++) {
                     if (strcmp(words[i], dictionary[j]) == 0) {
+
                         // setea la variable como true (1)
                         wordFoundInDictionary = 1;
-                        // imprime la palabra y su posicion
-                        printf("\n%s en posicion: %i en posicion esta repetida\n", words[i], j);
+
+                        // finaliza el bucle una vez se encontro un repetido
+                        break;
                     }
                 }
 
+                // variable en la cual ingresara si/no+\0 ingresado automaticamente
+                char addToDictionary[3];
+
                 // si no se encontro en el diccionario
                 // le pregunta al usario si desea agregarla
-                char addToDictionary[3];
                 if (wordFoundInDictionary == 0) {
-                    printf("\nNo esta en el dicionario, ingrese si o no si desea agregarla\n");
+                    printf("\nla palabra %s no esta en el dicionario, escriba si/no para agregarla ", words[i]);
+
+                    // toma la respuesta, y donde encuentre el salto de linea \n lo asigna por un \0 para indicar la terminacion
                     fgets(addToDictionary, sizeof(addToDictionary), stdin);
                     replaceWithArrayEnd(addToDictionary);
 
                     // limpia caracteres residuales en el input
                     clearInputBuffer();
 
-                    // si responde que si, agrega al ultimo lugar del diccionario
-                    if (strcmp(addToDictionary, "si") == 0) {
+                    // si responde que si, y el numero de palabras es menor al numero maximo de palabras -1, para el \0, se agrega al ultimo lugar del diccionario
+                    if (strcmp(addToDictionary, "si") == 0 && dictionaryCount < arrayLength - 1) {
                         printf("\nSe agrego al dicionario\n");
-                        if (dictionaryCount < arrayLength) {
-                            strcpy(dictionary[dictionaryCount], words[i]);
-                            printf("\n%s\n", dictionary[dictionaryCount]);
-                            dictionaryCount++;
-                        }
+
+                        // copia en el diccionario, en el ultimo logar registrado, la palabra en el array word
+                        strcpy(dictionary[dictionaryCount], words[i]);
+
+                        // se suma para saber cual es el ultimo lugar del diccionario
+                        dictionaryCount++;
+                    } else {
+                        printf("\nno se agrego al diccionario\n");
                     }
                 }
             }
         }
 
-        printf("diccionario actualizado");
+        // se imprime el diccionario actualizado una vez que se analizo la oracion ingresada por el uruario
+        printf("\ndiccionario actualizado:\n");
         for (int i = 0; i < dictionaryCount; i++) {
             printf("%s ", dictionary[i]);
         }
+
         // libera el espacio de la memoria
         for (int i = 0; i < wordCount; i++) {
             free(words[i]);
