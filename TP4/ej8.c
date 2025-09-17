2902950fe99c6b73b7db8221aa4dd159c51ed1f8
Leo <leonel.m.cata03@gmail.com>
2025-09-17 21:19:32 +0100

    tp4 ej8

--- TP4/ej8.c
+++ TP4/ej8.c
@@ -1,5 +1,10 @@
 /*
-Ejercicio 8. Modificá el programa del ejercicio E2TP1 para que use una función que reciba el ingreso por teclado del usuario y devuelva un arreglo de caracteres que represente el nombre
+Ejercicio 8. Modificá el programa del ejercicio E2TP1 para que use una función que reciba
+el ingreso por teclado del usuario y devuelva un arreglo de caracteres que represente el nombre
+1. char[] ingresarNombre();
+2. char* ingresarNombre();
+3. ingresarNombre(char *nombre);
+4. ingresarNombre(char nombre[]);
 */
 
 #include <stdio.h>
@@ -8,16 +13,29 @@ Ejercicio 8. Modificá el programa del ejercicio E2TP1 para que use una función
 // funcion que pide el nombre y lo devuelve
 void ingresarNombre(char* nombre)
 {
-    printf("cual es tu nombre?\n");
+    printf("cual es tu nombre?char*nombre[]\n");
     scanf("%s", nombre);
 };
+
+void ingresarNombre1(char nombre[])
+{
+    printf("cual es tu nombre? (char nombre[])\n");
+
+    scanf("%s", nombre);
+}
 
 int main()
 {
     // inicializacion de la variable e invocacion de la funcion
     char nombre[nameMaxLength];
+    char nombre2[nameMaxLength];
     ingresarNombre(nombre);
+    ingresarNombre1(nombre2);
 
     // imprime el saludo
-    printf("hola: %s", nombre);
+    printf("hola: %s, hola2:%s", nombre, nombre2);
 }
+
+// 3 y 4 son correctos, 1 y 2 es sintaxis invalida
+// char *nombre pasa la direccion de memoria del primer elemento
+// char nombre[] hace lo mismo por lo que ambas son validas
